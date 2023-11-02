bfbe638e8efe3f509382490a6f44f5dd306bc6d7
Paras Singh <149015075+Parass2802@users.noreply.github.com>
2023-11-03 00:03:34 +0530

    Lax-Friedrichs-scheme

---
+++ LF scheme.py
@@ -0,0 +1,68 @@
+# 1D Burgers eqn for flow with a step function as IC
+#Lax-Friedrichs scheme
+
+import numpy
+from matplotlib import pyplot
+
+#domain def
+nx=51
+tmax=2
+xmax=4
+sigma=0.5
+sigma_an=2
+
+
+dx=xmax/(nx-1)
+dt=sigma*dx
+nt=int((tmax/dt)+1)
+
+
+dt_an = dx/0.5
+nt_an = int((tmax/dt_an)+1)
+
+
+x=numpy.linspace(0,xmax,nx)
+
+#IC
+u=numpy.zeros(nx)
+u[0:int(2/dx)+1]=1
+
+u_an=numpy.zeros(nx)
+u_an[0:int(2/dx)+1]=1
+
+#plotting IC
+pyplot.figure(figsize=(11,7),dpi=300)
+pyplot.plot(x,u,label='IC')
+
+
+#Numerical solution
+for n in range(nt):
+    un=u.copy()
+    Fn=(un**2)/2
+    
+    u[1:-1]=0.5*(un[0:-2]+un[2:])-(sigma/2)*(Fn[2:]-Fn[0:-2])
+    
+    
+    #BC
+    u[0]=1
+    u[-1]=0
+    
+    
+#Analytical Sol
+for n in range(nt_an):
+    un_an=u_an.copy()
+    u_an[1:]=un_an[0:-1]
+    
+    #BC
+    u_an[0]=1
+    u_an[-1]=0
+   
+#plotting results
+
+pyplot.plot(x,u,label='LF solution')
+pyplot.plot(x,u_an,'--',label='Analytical solution')
+pyplot.legend()
+pyplot.legend()
+pyplot.xlabel('x [m]')
+pyplot.ylabel('u [m/s]')
+pyplot.title('Lax-Friedrichs scheme')
